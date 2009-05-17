21f1ee0f71a0d0a326789db707825a61d0ec311b
vhj2002 <vhj2002@8fb5d510-e65f-11dd-bd7b-b12b6590754f>
2009-05-17 15:58:26 +0000

    WORKING

--- location/context_processors.py
+++ location/context_processors.py
@@ -4,8 +4,8 @@ from datetime import datetime
 def rides(request):
     if hasattr(request, 'user'):
         user = request.user
-        rides = Ride.objects.filter(Q(dateTime__gte=datetime.today().date()) | Q(everyDay=True),driver=user,).order_by('dateTime')[:2]
-        passengers = Passenger.objects.filter(Q(dateTime__gte=datetime.today().date()) | Q(everyDay=True),passenger=user).order_by('dateTime')[:2]
+        rides = Ride.objects.filter(Q(dateTime__gte=datetime.today().date()) | Q(everyDay=True),driver=user,).order_by('dateTime')[:1]
+        passengers = Passenger.objects.filter(Q(dateTime__gte=datetime.today().date()) | Q(everyDay=True),passenger=user).order_by('dateTime')[:1]
         return {
             'rides_for_right': rides,
             'passengers_for_right': passengers,
