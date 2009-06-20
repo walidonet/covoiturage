f9f7a5656404440191a7363ab00d7a26dc509e08
vhj2002 <vhj2002@8fb5d510-e65f-11dd-bd7b-b12b6590754f>
2009-06-20 15:13:58 +0000

    modif urls.py correction des patterns

--- news/urls.py
+++ news/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls.defaults import *
 from views import *
 
 urlpatterns = patterns('',
-    (r'^edit/(?P<news_id>\d)+/$', edit),
-    (r'^delete/(?P<news_id>\d)+/$', delete),
+    (r'^edit/(?P<news_id>\d+)/$', edit),
+    (r'^delete/(?P<news_id>\d+)/$', delete),
     (r'^add$', add),
-    (r'^show/(?P<news_id>\d)+/$',show),
+    (r'^show/(?P<news_id>\d+)/$',show),
     (r'^archives/', list_all),
 	(r'^$', news_view),
 )
